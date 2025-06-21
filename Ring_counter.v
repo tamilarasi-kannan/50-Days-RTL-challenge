5ce79858d9e9e9beab2abf8a846f92b4850f82b3
Tamilarasi Kannan <115526719+tamilarasi-kannan@users.noreply.github.com>
2025-06-21 15:45:19 +0530

    Update Ring_counter.v

--- Ring_counter.v
+++ Ring_counter.v
@@ -1,1 +1,13 @@
+module ring_counter(clk,reset,count);
+  parameter N = 4;
+  input clk,rst;
+  output reg [N-1:0] count;
 
+  always@(posedge clk)
+    if(reset) begin
+      count = {count[0],[N-1:1]count};
+  end
+  else begin
+    count = 1;
+  end
+endmodule
